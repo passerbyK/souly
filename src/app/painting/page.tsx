d3faab3717ee1da6a416baaa2a7b62d070ead0eb
XiangYi <azxcv1705@gmail.com>
2023-12-27 00:04:33 +0800

    feature: save painting

--- src/app/painting/page.tsx
+++ src/app/painting/page.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { ChromePicker } from "react-color";
 import type { ColorResult } from "react-color";
+import { toPng } from "html-to-image";
 
 import { useDraw } from "@/hooks/useDraw";
 import type { Draw } from "@/lib/types/shared_types";
@@ -11,9 +12,26 @@ export default function Home() {
   const [color, setColor] = useState<string>("#000");
   const [showPicker, setShowPicker] = useState(false);
   const { canvasRef, onMouseDown, clear } = useDraw(drawLine);
+  const elementRef = useRef<HTMLDivElement>(null);
 
   const handleColorIconClick = () => {
     setShowPicker(!showPicker);
+  };
+
+  const htmlToImageConvert = () => {
+    if (elementRef.current) {
+      toPng(elementRef.current, { cacheBust: false })
+        .then((dataUrl) => {
+          const link = document.createElement("a");
+          link.download = "my-image-name.png";
+          link.href = dataUrl;
+          console.log("link", link);
+          link.click();
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    }
   };
 
   function drawLine({ prevPoint, currentPoint, ctx }: Draw) {
@@ -43,26 +61,28 @@ export default function Home() {
           <div className="mb-2 text-center text-txt_4 md:mb-0 md:w-1/2">
             Today's Topic:{" "}
             <span className="max-w-full overflow-hidden whitespace-nowrap underline">
-              XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+              A dog running in the park
             </span>
           </div>
           <div className="text-gray-500">2023/12/09 {"(Sat.)"}</div>
         </div>
         <div className="my-4 flex justify-between gap-4">
-          <div className="relative w-full items-center rounded-2xl border-4 border-bdr_2 bg-white">
-            <canvas
-              ref={canvasRef}
-              onMouseDown={onMouseDown}
-              className="w-full rounded-2xl"
-            />
+          <div className="w-full items-center rounded-2xl border-4 border-bdr_2 ">
+            <div ref={elementRef} className="rounded-2xl bg-white">
+              <canvas
+                ref={canvasRef}
+                onMouseDown={onMouseDown}
+                className="w-full h-full rounded-2xl"
+              />
+            </div>
           </div>
-          <div className="flex w-full flex-col gap-4">
+          <div className="flex flex-col gap-4">
             <input
               type="text"
-              className="w-full rounded-2xl border-4 border-bdr_2 bg-brand p-2 text-xl"
+              className="grow w-full rounded-2xl border-4 border-bdr_2 bg-brand p-2 text-xl overflow-auto overflow-clip"
               placeholder="Type something..."
             />
-            <p className="text-md mt-2 text-center text-gray-500">
+            <p className="text-md my-2 text-center text-gray-500">
               View others’ painting after you accomplish your mission.
             </p>
           </div>
@@ -70,9 +90,9 @@ export default function Home() {
         <div className="md:2/5 flex">
           <div className="flex flex-col gap-10 pr-10">
             <div
-              className="rounded-full border-2 border-black p-2"
+              className={`rounded-full border-2 border-black p-2 cursor-pointer`}
               onClick={handleColorIconClick}
-              style={{ backgroundColor: color, cursor: "pointer" }}
+              style={{ backgroundColor: color}}
             >
               {showPicker && (
                 <ChromePicker
@@ -89,7 +109,7 @@ export default function Home() {
               Clear canvas
             </button>
           </div>
-          <button className="rounded-2xl border-4 border-bdr bg-btn_2 px-4 py-2 text-center text-3xl text-txt">
+          <button onClick={htmlToImageConvert} className="rounded-2xl border-4 border-bdr bg-btn_2 px-4 py-2 text-center text-3xl text-txt">
             POST
           </button>
         </div>
