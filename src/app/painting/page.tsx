446c75c5908df284ebabb49ffa4b2447f67b8bf7
XiangYi <azxcv1705@gmail.com>
2023-12-27 02:08:15 +0800

    fix canvas rendering

--- src/app/painting/page.tsx
+++ src/app/painting/page.tsx
@@ -1,37 +1,25 @@
 "use client";
 
-import { useRef, useState } from "react";
+import { useState } from "react";
+import { useSession } from "next-auth/react";
 import { ChromePicker } from "react-color";
 import type { ColorResult } from "react-color";
-import { toPng } from "html-to-image";
+
+import { useRouter } from "next/navigation";
 
 import { useDraw } from "@/hooks/useDraw";
 import type { Draw } from "@/lib/types/shared_types";
 
-export default function Home() {
+export default function Painting() {
+  const { status } = useSession();
+  const router = useRouter();
+
   const [color, setColor] = useState<string>("#000");
   const [showPicker, setShowPicker] = useState(false);
   const { canvasRef, onMouseDown, clear } = useDraw(drawLine);
-  const elementRef = useRef<HTMLDivElement>(null);
 
   const handleColorIconClick = () => {
     setShowPicker(!showPicker);
-  };
-
-  const htmlToImageConvert = () => {
-    if (elementRef.current) {
-      toPng(elementRef.current, { cacheBust: false })
-        .then((dataUrl) => {
-          const link = document.createElement("a");
-          link.download = "my-image-name.png";
-          link.href = dataUrl;
-          console.log("link", link);
-          link.click();
-        })
-        .catch((err) => {
-          console.error(err);
-        });
-    }
   };
 
   function drawLine({ prevPoint, currentPoint, ctx }: Draw) {
@@ -53,67 +41,69 @@ export default function Home() {
     ctx.fill();
   }
 
-  return (
-    <main className="flex h-screen min-h-screen flex-col items-center bg-brand">
-      <div className="h-1/6 w-full"></div>
-      <div className="w-full flex-col items-center justify-center bg-brand_2 px-4 md:px-12">
-        <div className="mt-4 flex w-full flex-col items-center justify-between gap-4 px-4 text-4xl md:flex-row">
-          <div className="mb-2 text-center text-txt_4 md:mb-0 md:w-1/2">
-            Today's Topic:{" "}
-            <span className="max-w-full overflow-hidden whitespace-nowrap underline">
-              A dog running in the park
-            </span>
+  if (status !== "authenticated") {
+    router.push("/auth/login");
+  } else {
+    return (
+      <main className="flex h-screen min-h-screen flex-col items-center bg-brand">
+        <div className="h-1/6 w-full"></div>
+        <div className="w-full h-full flex-col items-center justify-center bg-brand_2 px-4 md:px-12">
+          <div className="mt-4 flex w-full items-center gap-4 px-4 text-4xl ">
+            <div className="flex-wrap grow w-3/4 my-4 text-center text-txt_4 md:w-1/2">
+              Today's Topic:{" "}
+              <span className="max-w-full overflow-hidden whitespace-nowrap underline">
+                A dog running in the park
+              </span>
+            </div>
+            <div className="flex-wrap w-1/4 text-gray-500">2023/12/09 {"(Sat.)"}</div>
           </div>
-          <div className="text-gray-500">2023/12/09 {"(Sat.)"}</div>
-        </div>
-        <div className="my-4 flex justify-between gap-4">
-          <div className="w-full items-center rounded-2xl border-4 border-bdr_2 ">
-            <div ref={elementRef} className="rounded-2xl bg-white">
+          <div className="mb-4 flex gap-4">
+            <div className="relative w-[1000px] h-[400px] items-center rounded-2xl border-4 border-bdr_2 bg-white">
               <canvas
                 ref={canvasRef}
                 onMouseDown={onMouseDown}
                 className="w-full h-full rounded-2xl"
               />
             </div>
-          </div>
-          <div className="flex flex-col gap-4">
-            <input
-              type="text"
-              className="grow w-full rounded-2xl border-4 border-bdr_2 bg-brand p-2 text-xl overflow-auto overflow-clip"
-              placeholder="Type something..."
-            />
-            <p className="text-md my-2 text-center text-gray-500">
-              View others’ painting after you accomplish your mission.
-            </p>
-          </div>
-        </div>
-        <div className="md:2/5 flex">
-          <div className="flex flex-col gap-10 pr-10">
-            <div
-              className={`rounded-full border-2 border-black p-2 cursor-pointer`}
-              onClick={handleColorIconClick}
-              style={{ backgroundColor: color}}
-            >
-              {showPicker && (
-                <ChromePicker
-                  color={color}
-                  onChange={(e: ColorResult) => setColor(e.hex)}
-                />
-              )}
+            <div className="flex flex w-1/3 flex-col gap-4">
+              <input
+                type="text"
+                className="flex grow w-full rounded-2xl border-4 border-bdr_2 bg-brand p-2 text-xl"
+                placeholder="Type something..."
+              />
+              <p className="text-md mt-2 text-center text-gray-500">
+                View others’ painting after you accomplish your mission.
+              </p>
             </div>
-            <button
-              type="button"
-              className="rounded-md border border-black p-2"
-              onClick={clear}
-            >
-              Clear canvas
+          </div>
+          <div className="flex">
+            <div className="grow flex gap-10 pr-10">
+              <button
+                type="button"
+                className="rounded-md border border-black p-2"
+                onClick={clear}
+              >
+                Clear canvas
+              </button>
+              <div
+                className="w-[50px] h-[50px] self-center rounded-full border-2 border-black p-2"
+                onClick={handleColorIconClick}
+                style={{ backgroundColor: color, cursor: "pointer" }}
+              >
+                {showPicker && (
+                  <ChromePicker
+                    color={color}
+                    onChange={(e: ColorResult) => setColor(e.hex)}
+                  />
+                )}
+              </div>
+            </div>
+            <button className="rounded-2xl border-4 border-bdr bg-btn_2 px-4 py-2 text-center text-3xl text-txt">
+              POST
             </button>
           </div>
-          <button onClick={htmlToImageConvert} className="rounded-2xl border-4 border-bdr bg-btn_2 px-4 py-2 text-center text-3xl text-txt">
-            POST
-          </button>
         </div>
-      </div>
-    </main>
-  );
+      </main>
+    );
+  }
 }
