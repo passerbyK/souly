cc8f6ee8241a6cf64ffbe3c5d7cf34213fbe9eae
vivicheng031 <80449867+vivicheng031@users.noreply.github.com>
2023-12-30 14:01:15 +0800

    add feedback (#52)
    * add feedback

--- src/app/social/layout.tsx
+++ src/app/social/layout.tsx
@@ -7,11 +7,11 @@ type Props = {
 async function Social({ children }: Props) {
   return (
     // overflow-hidden for parent to hide scrollbar
-    <main className="flex h-screen w-full flex-col justify-center overflow-y-auto md:overflow-hidden">
+    <main className="flex h-screen w-full flex-col justify-center overflow-y-auto">
       {/* overflow-y-scroll for child to show scrollbar */}
       <div className="h-1/6 w-full"></div>
       <div className="flex-rows h-5/6 bg-brand_2 lg:flex">
-        <nav className="h-[280px] min-w-min flex-col justify-start overflow-y-auto border-r bg-nav lg:my-0 lg:h-full lg:w-1/5">
+        <nav className="h-[280px] min-w-min flex-col justify-start overflow-y-auto border-r bg-nav lg:my-0 lg:h-full lg:w-1/3">
           <FriendList />
         </nav>
         {/* overflow-y-scroll for child to show scrollbar */}
