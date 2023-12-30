6983aea94f78718489dddece91669ba8e76a7ce2
PasserbyK <120707256+passerbyK@users.noreply.github.com>
2023-12-30 12:23:13 +0800

    UI: add homepage image & hover button (#48)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,11 +14,11 @@ export default function Home() {
       <div className="h-[200px] w-full"></div>
       <div className="m-4 flex h-full w-full flex-wrap items-center justify-center gap-20 p-8">
         <Image
-          src="/souly.svg"
+          src="/Homepage_new.png"
           alt="Souly Logo"
           className="mb-4 w-full rounded-2xl border-4 border-bdr lg:mb-0 lg:mr-4 lg:w-2/5"
-          width={25}
-          height={25}
+          width={3000}
+          height={3000}
         />
         <div className="mx-4 flex flex-col items-center gap-4 lg:w-2/5">
           <h1 className="mb-16 text-center text-6xl font-bold text-description">
@@ -36,7 +36,7 @@ export default function Home() {
           </p>
           <Link
             href="/auth/signup"
-            className="m-2 rounded-2xl border-4 border-bdr bg-btn px-4 py-2 text-center text-3xl text-txt"
+            className="m-2 rounded-2xl border-4 border-bdr bg-btn px-4 py-2 text-center text-3xl text-txt hover:bg-description/80"
           >
             Start Painting
           </Link>
